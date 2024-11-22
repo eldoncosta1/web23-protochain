5ee14e1e5ceafcb9aaa363baa2fec2943bb63bec
Eldon Costa <eldoncosta1@gmail.com>
2024-11-22 15:24:17 -0300

    blockchain server tests are added

--- src/server/blockChainServer.test.ts
+++ src/server/blockChainServer.test.ts
@@ -1,19 +1,66 @@
 import request from "supertest";
-import { app } from "./app";
 
-vi.mock("./block", async () => {
+vi.mock("../block", async () => {
   return await import("../__mocks__/block.mock");
 });
 
-vi.mock("./blockchain", async () => {
+vi.mock("../blockchain", async () => {
   return await import("../__mocks__/blockchain.mock");
 });
 
+import { app } from "./blockChainServer";
+import { Block } from "src/block";
+
 describe("BlockChainServer Test", () => {
-  it("GET /status", async () => {
+  it("GET /status - should return status", async () => {
     const response = await request(app).get("/status");
 
     expect(response.status).toEqual(200);
     expect(response.body.isValid._success).toBe(true);
+  });
+
+  it("GET /status/:index - should get genesis", async () => {
+    const response = await request(app).get("/blocks/0");
+
+    expect(response.status).toEqual(200);
+    expect(response.body.index).toEqual(0);
+  });
+
+  it("GET /blocks/:hash - should get block", async () => {
+    const response = await request(app).get("/blocks/abc");
+
+    expect(response.status).toEqual(200);
+    expect(response.body.hash).toEqual("abc");
+  });
+
+  it("GET /blocks/:index - should NOT get block", async () => {
+    const response = await request(app).get("/blocks/-1");
+
+    expect(response.status).toEqual(404);
+  });
+
+  it("POST /blocks - should add block", async () => {
+    const block = new Block({
+      index: 1,
+    } as unknown as Block);
+    const response = await request(app).post("/blocks/").send(block);
+
+    expect(response.status).toEqual(201);
+    expect(response.body.index).toEqual(1);
+  });
+
+  it("POST /blocks - should NOT add block (empty)", async () => {
+    const response = await request(app).post("/blocks/").send({});
+
+    expect(response.status).toEqual(422);
+  });
+
+  it("POST /blocks - should NOT add block (invalid)", async () => {
+    const block = new Block({
+      index: -1,
+    } as unknown as Block);
+    const response = await request(app).post("/blocks/").send(block);
+
+    expect(response.status).toEqual(400);
   });
 });
